82510815be3bb90c47e46bf2e7ed38cbedfab072
Trent Hoppe <trhoppe@microsoft.com>
2020-01-21 19:07:48 -0800

    Added navlinks

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import MainHeader from './MainHeader'
+import NavLinks from './NavLinks'
 
 import './MainNavigation.css'
 
@@ -17,7 +18,7 @@ const MainNavigation = () => {
                 <Link to="/">Your Places</Link>
             </h1>
             <nav>
-                ...
+                <NavLinks />
             </nav>
         </MainHeader>
     )
