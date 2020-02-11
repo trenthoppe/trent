a30c36ca56c406db8e28ff43c5dfe5610c7d4517
trenthoppe <hoppetrent06@gmail.com>
2020-02-10 21:11:04 -0800

    Added rotation to cube

---
+++ src/landing/components/Bubble.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Bubble = props => {
+    
+}
+
+export default Bubble;
--- src/landing/components/Canvas.js
+++ src/landing/components/Canvas.js
@@ -1,33 +1,40 @@
 import React from 'react';
 import P5Wrapper from 'react-p5-wrapper';
 
-const Sketch = props => {
+const Sketch = p5 => {
     let rotation = 0;
   
-    props.setup = function () {
-        props.createCanvas(600, 400, props.WEBGL);
+    p5.setup = () => {
+        p5.createCanvas(p5.windowWidth, p5.windowHeight, p5.WEBGL);
     };
+
+    p5.windowResized = () => {
+        p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
+    }
   
-    props.myCustomRedrawAccordingToNewPropsHandler = function (props) {
+    p5.myCustomRedrawAccordingToNewPropsHandler = props => {
         if (props.rotation){
             rotation = props.rotation * Math.PI / 180;
         }
     };
   
-    props.draw = function () {
-        props.background(100);
-        props.normalMaterial();
-        props.noStroke();
-        props.push();
-        props.rotateY(rotation);
-        props.box(100);
-        props.pop();
+    p5.draw = function () {
+        p5.background(200);
+        p5.normalMaterial();
+        p5.noStroke();
+        p5.push();
+        p5.rotateY(rotation);
+        p5.rotateX(rotation);
+        p5.box(100);
+        p5.pop();
+        p5.frameRate(50);
+        rotation += .01;
     };
 };
 
 const Canvas = props => {
-    
 
+    
     return <P5Wrapper sketch={Sketch} rotation={200}></P5Wrapper>
 }
 
